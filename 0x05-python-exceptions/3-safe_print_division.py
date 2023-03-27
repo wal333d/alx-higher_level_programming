19706a1d17a35058ffd73d09c84d6042fea12867
wal333d <walleednas33r@gmail.com>
2023-03-27 15:49:52 -0700

    corrections

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,19 +1,11 @@
-
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for i in range(list_length):
-        try:
-            result = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
-            print("wrong type")
-            result = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            result = 0
-        except IndexError:
-            print("out of range")
-            result = 0
-        finally:
-            new_list.append(result)
-    return new_list
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
